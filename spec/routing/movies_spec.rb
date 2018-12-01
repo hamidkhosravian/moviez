40b3372c1b169fed45f9095c17c358c1a4de00ed
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-12-01 23:30:56 +0330

    add routes and tests of routes

---
+++ spec/routing/movies_spec.rb
@@ -0,0 +1,11 @@
+describe Api::V1::MoviesController, :type => :routing do
+  describe 'movies' do
+    it 'index' do
+      expect(get: 'api/v1/movies').to route_to(controller: 'api/v1/movies', action: 'index')
+    end
+
+    it 'show' do
+      expect(get: 'api/v1/movies/1').to route_to(controller: 'api/v1/movies', action: 'show', id: "1")
+    end
+  end
+end
