de9c2d9a6c48b050b6365e72b9de903d8cbda804
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-12-01 21:19:06 +0330

    add movie model tests

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :description }
+  end
 end
