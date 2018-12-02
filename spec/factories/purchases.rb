3167a773ca802f1b70713ace230ff7bda30cb384
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-12-02 17:48:43 +0330

    add     name %w[HD SD].sample

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :purchase do
     name %w[HD SD].sample
+    type %w[HD SD].sample
     description Faker::Movie.quote
     price Faker::Number.decimal(2)
     purchaseable { nil }
